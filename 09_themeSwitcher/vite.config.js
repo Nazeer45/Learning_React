f220eb9fb9b74d9c29fa5599c21f52d1513fd24c
Nazeer <nazeerazam45@gmail.com>
2025-08-22 22:09:53 +0530

    Another way of using useContext

---
+++ 09_themeSwitcher/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  darkMode: "class",
+  plugins: [react(), tailwindcss()],
+})
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   darkMode: "class",
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+
+
